a6d50043b45132bb7cb49cb0e4a80cf8ff0ff5c7
LiamMesarec <mesarec.liam@gmail.com>
2022-10-31 01:23:25 +0100

    added tests

---
+++ include/sensors/Device.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+namespace sensors{
+struct Device
+{
+    enum class Type
+    {
+        CPU, GPU
+    };
+
+    std::string name;
+    Type type;
+};
+}
---
+++ include/sensors/sensors.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+#include "Device.hpp"
+
+namespace sensors
+{
+    std::vector<Device> getDevices(Device::Type)
+}
