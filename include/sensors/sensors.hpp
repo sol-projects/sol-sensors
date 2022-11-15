b0aebc15f7b0ff9a74f3be9635989095b2363dd8
LiamMesarec <mesarec.liam@gmail.com>
2022-11-15 12:58:37 +0100

    vram comments

--- include/sensors/sensors.hpp
+++ include/sensors/sensors.hpp
@@ -6,20 +6,27 @@ namespace sensors
 {
     std::vector<Device> getDevices(Device::Type type);
 
-    //CPU: returns % Average CPU Usage across all cores
-    //     precision: xx.x %
-    //RAM: returns total RAM usage in GB
-    //     precision: xx.x GB
-    //GPU:
-    //
+    /*
+    CPU: returns % Average CPU Usage across all cores
+         precision: xx.x %
+    RAM: returns total RAM usage in GB
+         precision: xx.x GB
+    GPU:
+
+    VRAM: returns total RAM usagein GB
+          precision: xx.x GB
+    */
     int getLoad(const Device& device);
 
-    //CPU: returns temperature
-    //     precision: xx C
-    //RAM: returns 0
-    //
-    //GPU: returns temperature in celsius
-    //     precision: xx C
+    /*
+    CPU: returns temperature in celsius
+         precision: xx C
+    RAM: returns 0
+    GPU: returns temperature in celsius
+         precision: xx C
+    VRAM: returns temperature in celsius
+         precision: xx C
+    */
     int getTemp(const Device& device);
 }
 
