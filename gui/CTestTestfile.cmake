bcfdac55efe51264959c625f3cff814c2906efad
LiamMesarec <mesarec.liam@gmail.com>
2022-11-24 02:56:22 +0100

    deleted accidental commits

--- gui/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liam/Desktop/rpo/gui
-# Build directory: /home/liam/Desktop/rpo/gui
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
