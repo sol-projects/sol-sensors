d7c64ad1289db6b2922480eed7eca037b74a9a8f
LiamMesarec <mesarec.liam@gmail.com>
2022-12-15 11:00:59 +0100

    modulation

--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
-#include "nogui/include/nogui/nogui.hpp"
-#include "tests/config.hpp"
-#include "tests/csv.hpp"
-#include "tests/sensors.hpp"
-#include "tests/stats.hpp"
-#include "tests/nogui.hpp"
-#include <DOCTEST/doctest.h>
-#include <cargs.h>
-#include "nogui/include/nogui/options.hpp"
+#ifdef NOGUI
+    #include "nogui/include/nogui/nogui.hpp"
+    #include "nogui/include/nogui/options.hpp"
+    #ifdef TESTS
+        #include "tests/nogui.hpp"
+    #endif
+#endif
+
+#ifdef TESTS
+    #include "tests/config.hpp"
+    #include "tests/csv.hpp"
+    #include "tests/sensors.hpp"
+    #include "tests/stats.hpp"
+    #include <DOCTEST/doctest.h>
+#endif
 
 int main(int argc, char* argv[])
 {
-    nogui::run(argc, argv);
+    #ifdef NOGUI
+        nogui::run(argc, argv);
+    #endif
 
-    doctest::Context doctestContext;
-    int result = doctestContext.run();
+    #ifdef TESTS
+        doctest::Context doctestContext;
+        int result = doctestContext.run();
 
-    if (doctestContext.shouldExit())
-    {
+        if (doctestContext.shouldExit())
+        {
+            return result;
+        }
+
         return result;
-    }
+    #endif
 
-    return result;
+    return 0;
 }
