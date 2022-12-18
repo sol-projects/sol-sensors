f1d57c7d65320ca863de65084a7f53ed2bf3de46
LiamMesarec <mesarec.liam@gmail.com>
2022-12-18 03:15:55 +0100

    examples

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,7 +1,13 @@
 #include <sensors/sensors.hpp>
-#include <sensors/Devices.hpp>
+#include <sensors/Device.hpp>
+#include <iostream>
 
 int main()
 {
-    auto devices = sensors::getDevices(sensors::Device::Type::CPU);
+    auto devices = sensors::getDevices(sensors::Device::Type::Any);
+
+    for(auto& device : devices)
+    {
+        std::cout << device.name << '\n';
+    }
 }
